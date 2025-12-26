26a1085931f2ea1cb126e54052af9e09edddaf83
Yorkinjon10 <abdukhalilovyorkinjon2025@gmail.com>
2025-12-26 12:17:54 +0500

    minor update

--- leetcode/1277.Count Square Submatrices with All Ones.js
+++ leetcode/1277.Count Square Submatrices with All Ones.js
@@ -3,5 +3,44 @@
  * @return {number}
  */
 var countSquares = function(matrix) {
-    
+   const rows = matrix.length;
+   const cols = matrix[0].length;
+   const dp = Array.from({ length: rows }, () => Array(cols).fill(0));
+   let total = 0;
+   console.log(dp);
+
+   for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) { 
+         if (matrix[i][j] === 1) {
+            if (i === 0 || j === 0) {
+               dp[i][j] = 1;
+            } else {
+               dp[i][j] = Math.min(
+                  dp[i - 1][j],
+                  dp[i][j - 1],
+                  dp[i - 1][j - 1]
+               ) + 1;
+            }
+            total += dp[i][j];
+         }
+      }
+   }
+   return total;
 };
+
+matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+countSquares(matrix);
+
+// matrix = 
+// [
+//   [1,0,1],
+//   [1,1,0],
+//   [1,1,0]
+// ]
+
+// countSquares(matrix);
